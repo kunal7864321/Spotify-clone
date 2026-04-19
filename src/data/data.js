bbfa206166eb95954e3da96ff2ebd1125f644318
kunal kumar jha <kunalkumarjha@Kunals-MacBook-Pro-2.local>
2026-04-19 23:11:45 +0530

    data: add songs, artists, albums, playlists and helper functions

---
+++ src/data/data.js
@@ -0,0 +1,457 @@
+export const ARTISTS = [
+    {
+        id:"a1",
+        name:"Arijit Singh",
+        image:"https://upload.wikimedia.org/wikipedia/commons/thumb/1/1e/Arijit_Singh_at_2015_Filmfare_Awards.jpg/220px-Arijit_Singh_at_2015_Filmfare_Awards.jpg",
+        monthlyListeners:"50M",
+        genres:["Bollywood", "Romantic", "Pop"],
+    },
+    {
+        id:"a2",
+        name:"A.R. Rahman",
+        image:"https://upload.wikimedia.org/wikipedia/commons/thumb/1/1e/A.R._Rahman_at_2015_Filmfare_Awards.jpg/220px-A.R._Rahman_at_2015_Filmfare_Awards.jpg",
+        monthlyListeners:"40M",
+        genres:["Bollywood", "Pop", "Classical"],
+    },
+    {
+        id:"a3",
+        name:"Shreya Ghoshal",
+        image:"https://upload.wikimedia.org/wikipedia/commons/thumb/1/1e/Shreya_Ghoshal_at_2015_Filmfare_Awards.jpg/220px-Shreya_Ghoshal_at_2015_Filmfare_Awards.jpg",
+        monthlyListeners:"40M",
+        genres:["Bollywood", "Pop", "Classical","Romantic"],
+
+
+    },
+    {
+        id:"a4",
+        name:"Sonu Nigam",
+        image:"https://en.wikipedia.org/wiki/Sonu_Nigam_discography#/media/File:Tri_Nation_Mega_Concert_Sonu_Nigam_(8388639915).jpg",
+        monthlyListeners:"29M",
+        genres:["Romantic", "Bollywood", "Pop"],
+    },
+    {
+        id:"a5",
+        name:"Jubin Nautiyal",
+        image:"https://upload.wikimedia.org/wikipedia/commons/thumb/1/1e/Jubin_Nautiyal_at_2015_Filmfare_Awards.jpg/220px-Jubin_Nautiyal_at_2015_Filmfare_Awards.jpg",
+        monthlyListeners:"25M",
+        genres:["Bollywood", "Romantic", "Pop"],
+    },
+    {
+        id:"a6",
+        name:"Neha Kakkar",
+        image:"https://upload.wikimedia.org/wikipedia/commons/thumb/1/1e/Neha_Kakkar_at_2015_Filmfare_Awards.jpg/220px-Neha_Kakkar_at_2015_Filmfare_Awards.jpg",
+        monthlyListeners:"20M",
+        genres:["Bollywood", "Pop", "Romantic"],        
+    },
+    {
+        id:"a7",
+        name:"Badshah",
+        image:"https://upload.wikimedia.org/wikipedia/commons/thumb/1/1e/Badshah_at_2015_Filmfare_Awards.jpg/220px-Badshah_at_2015_Filmfare_Awards.jpg",
+        monthlyListeners:"15M",
+        genres:["Bollywood", "Pop", "Hip-Hop"],
+
+    },
+    {
+        id:"a8",
+        name:"Guru Randhawa",
+        image:"https://upload.wikimedia.org/wikipedia/commons/thumb/1/1e/Guru_Randhawa_at_2015_Filmfare_Awards.jpg/220px-Guru_Randhawa_at_2015_Filmfare_Awards.jpg",
+        monthlyListeners:"10M",
+        genres:["Bollywood", "Pop", "Romantic"],
+
+    },
+    {
+        
+        id:"a9",
+        name:"Atif Aslam",
+        image:"https://upload.wikimedia.org/wikipedia/commons/thumb/1/1e/Atif_Aslam_at_2015_Filmfare_Awards.jpg/220px-Atif_Aslam_at_2015_Filmfare_Awards.jpg",
+        monthlyListeners:"8M",
+        genres:["Bollywood", "Romantic", "Pop"],
+    }
+]
+
+export const ALBUMS = [
+    {
+        id:"al1",
+        name:"Best of Arijit Singh",
+        artistId:"a1",
+        artistName:"Arijit Singh",
+        image:"https://upload.wikimedia.org/wikipedia/commons/thumb/1/1e/Arijit_Singh_at_2015_Filmfare_Awards.jpg/220px-Arijit_Singh_at_2015_Filmfare_Awards.jpg",
+        year: 2020,
+        songs:[]
+    },
+
+    {
+        id:"al2",
+        name:"Best of A.R. Rahman",
+        artistId:"a2",
+        artistName:"A.R. Rahman",
+        image:"https://upload.wikimedia.org/wikipedia/commons/thumb/1/1e/A.R._Rahman_at_2015_Filmfare_Awards.jpg/220px-A.R._Rahman_at_2015_Filmfare_Awards.jpg",
+        year: 2019,
+        songs:[]
+    },
+    {
+        id:"al3",
+        name:"Best of Shreya Ghoshal",
+        artistId:"a3",
+        artistName:"Shreya Ghoshal",
+        image:"https://upload.wikimedia.org/wikipedia/commons/thumb/1/1e/Shreya_Ghoshal_at_2015_Filmfare_Awards.jpg/220px-Shreya_Ghoshal_at_2015_Filmfare_Awards.jpg",
+        year: 2018,
+        songs:[]
+    },
+    {
+        id:"al4",
+        name:"Best of Sonu Nigam",
+        artistId:"a4",
+        artistName:"Sonu Nigam",
+        image:"https://en.wikipedia.org/wiki/Sonu_Nigam_discography#/media/File:Tri_Nation_Mega_Concert_Sonu_Nigam_(8388639915).jpg",
+        year: 2017,
+        songs:[]
+    },
+    {
+        id:"al5",
+        name:"Jubin Nautiyal Hits",
+        artistId:"a5",
+        artistName:"Jubin Nautiyal",
+        image:"https://upload.wikimedia.org/wikipedia/commons/thumb/1/1e/Jubin_Nautiyal_at_2015_Filmfare_Awards.jpg/220px-Jubin_Nautiyal_at_2015_Filmfare_Awards.jpg",
+        year: 2016,
+        songs:[]
+    },
+    {id:"al6",
+        name:"Neha Kakkar Hits",
+        artistId:"a6",
+        artistName:"Neha Kakkar",
+        image:"https://upload.wikimedia.org/wikipedia/commons/thumb/1/1e/Neha_Kakkar_at_2015_Filmfare_Awards.jpg/220px-Neha_Kakkar_at_2015_Filmfare_Awards.jpg",
+        year: 2015,
+        songs:[]
+    },
+    {
+        id:"al7",
+        name:"Badshah Hits",
+        artistId:"a7",
+        artistName:"Badshah",
+        image:"https://upload.wikimedia.org/wikipedia/commons/thumb/1/1e/Badshah_at_2015_Filmfare_Awards.jpg/220px-Badshah_at_2015_Filmfare_Awards.jpg",
+        year: 2014,
+        songs:[]
+    },
+    {
+        id:"al8",
+        name:"Guru Randhawa Hits",
+        artistId:"a8",
+        artistName:"Guru Randhawa",
+        image:"https://upload.wikimedia.org/wikipedia/commons/thumb/1/1e/Guru_Randhawa_at_2015_Filmfare_Awards.jpg/220px-Guru_Randhawa_at_2015_Filmfare_Awards.jpg",
+        year: 2013,
+        songs:[]
+    },
+    {
+        id:"al9",
+        name:"Atif Aslam Hits",
+        artistId:"a9",
+        artistName:"Atif Aslam",
+        image:"https://upload.wikimedia.org/wikipedia/commons/thumb/1/1e/Atif_Aslam_at_2015_Filmfare_Awards.jpg/220px-Atif_Aslam_at_2015_Filmfare_Awards.jpg",
+        year: 2012,
+        songs:[]
+    }
+
+    
+    
+];
+export const SONGS = [
+    {
+    id: "s1",
+    title: "Tum Hi Ho",
+    artistId: "a1",
+    artistName: "Arijit Singh",
+    albumId: "al1",
+    albumName: "Best of Arijit Singh",
+    duration: "4:21",    
+    image: "https://upload.wikimedia.org/wikipedia/commons/thumb/1/1e/Arijit_Singh_at_2015_Filmfare_Awards.jpg/220px-Arijit_Singh_at_2015_Filmfare_Awards.jpg",
+  },
+  {
+    id: "s2",
+    title: "Channa Mereya",
+    artistId: "a1",
+    artistName: "Arijit Singh",
+    albumId: "al1",
+    albumName: "Best of Arijit Singh",
+    duration: "4:56",    
+    image: "https://upload.wikimedia.org/wikipedia/commons/thumb/1/1e/Arijit_Singh_at_2015_Filmfare_Awards.jpg/220px-Arijit_Singh_at_2015_Filmfare_Awards.jpg",
+  },
+  {
+    id: "s3",
+    title: "Raabta",
+    artistId: "a1",
+    artistName: "Arijit Singh",
+    albumId: "al1",
+    albumName: "Best of Arijit Singh",
+    duration: "4:12",    
+    image: "https://upload.wikimedia.org/wikipedia/commons/thumb/1/1e/Arijit_Singh_at_2015_Filmfare_Awards.jpg/220px-Arijit_Singh_at_2015_Filmfare_Awards.jpg",
+  },
+  {
+    id: "s4",
+    title: "Agar Tum Saath Ho",
+    artistId: "a1",
+    artistName: "Arijit Singh",
+    albumId: "al1",
+    albumName: "Best of Arijit Singh",
+    duration: "4:45",    
+    image: "https://upload.wikimedia.org/wikipedia/commons/thumb/1/1e/Arijit_Singh_at_2015_Filmfare_Awards.jpg/220px-Arijit_Singh_at_2015_Filmfare_Awards.jpg",
+  },
+  {
+    id: "s5",
+    title: "Jai Ho",
+    artistId: "a2",
+    artistName: "A.R. Rahman",
+    albumId: "al2",
+    albumName: "Best of A.R. Rahman",
+    duration: "5:00",    
+    image: "https://upload.wikimedia.org/wikipedia/commons/thumb/1/1e/A.R._Rahman_at_2015_Filmfare_Awards.jpg/220px-A.R._Rahman_at_2015_Filmfare_Awards.jpg",
+  },
+  {
+    id: "s6",
+    title: "Chaiyya Chaiyya",
+    artistId: "a2",
+    artistName: "A.R. Rahman",
+    albumId: "al2",
+    albumName: "Best of A.R. Rahman",
+    duration: "4:30",    
+    image: "https://upload.wikimedia.org/wikipedia/commons/thumb/1/1e/A.R._Rahman_at_2015_Filmfare_Awards.jpg/220px-A.R._Rahman_at_2015_Filmfare_Awards.jpg",
+  },
+  {
+    id: "s7",
+    title: "Tere Bina",
+    artistId: "a2",
+    artistName: "A.R. Rahman",
+    albumId: "al2",
+    albumName: "Best of A.R. Rahman",
+    duration: "4:15",    
+    image: "https://upload.wikimedia.org/wikipedia/commons/thumb/1/1e/A.R._Rahman_at_2015_Filmfare_Awards.jpg/220px-A.R._Rahman_at_2015_Filmfare_Awards.jpg",
+  },
+  {
+    id:"s8",
+    title:"Teri meri",
+    artistId:"a3",
+    artistName:"Shreya Ghoshal",
+    albumId:"al3",
+    albumName:"Best of Shreya Ghoshal",
+    duration:"4:20",
+    image:"https://upload.wikimedia.org/wikipedia/commons/thumb/1/1e/Shreya_Ghoshal_at_2015_Filmfare_Awards.jpg/220px-Shreya_Ghoshal_at_2015_Filmfare_Awards.jpg"
+  },
+  {
+    id:"s9",
+    title:"Sun Raha Hai",
+    artistId:"a3",
+    artistName:"Shreya Ghoshal",
+    albumId:"a3",
+    albumName:"Best of Shreya Ghoshal",
+    duration:"4:25",
+    image:""
+},
+{
+    id:"s10",
+    title:"Ishq Shayari",
+    artistId:"a3",
+    artistName:"Shreya Ghoshal",
+    albumId:"a3",
+    albumName:"Best of Shreya Ghoshal",
+    duration:"3:04",
+    image:""
+
+},
+{
+    id:"s11",
+    title:"Kal ho naa ho",
+    artistId:"a4",
+    artistName:"Sonu Nigam",
+    albumId:"a4",
+    albumName:"Best of Sonu Nigam",
+    duration:"4:50",
+    image:"https://en.wikipedia.org/wiki/Sonu_Nigam_discography#/media/File:Tri_Nation_Mega_Concert_Sonu_Nigam_(8388639915).jpg"
+},
+{
+    id:"s12",
+    title:"Abhi Mujh Mein Kahin",
+    artistId:"a4",
+    artistName:"Sonu Nigam",
+    albumId:"a4",
+    albumName:"Best of Sonu Nigam",
+    duration:"5:10",
+    image:"https://en.wikipedia.org/wiki/Sonu_Nigam_discography#/media/File:Tri_Nation_Mega_Concert_Sonu_Nigam_(8388639915).jpg"        
+
+},
+{
+    id:"s13",
+    title:"Suraj Hua Maddham",
+    artistId:"a4",
+    artistName:"Sonu Nigam",
+    albumId:"a4",
+    albumName:"Best of Sonu Nigam",
+    duration:"4:40",
+    image:"https://en.wikipedia.org/wiki/Sonu_Nigam_discography#/media/File:Tri_Nation_Mega_Concert_Sonu_Nigam_(8388639915).jpg"
+},
+{
+    id:"s14",
+    title:"Lut Gaye",
+    artistId:"a5",
+    artistName:"Jubin Nautiyal",
+    albumId:"a5",
+    albumName:"Jubin Nautiyal Hits",
+    duration:"4:30",
+    image:"https://upload.wikimedia.org/wikipedia/commons/thumb/1/1e/Jubin_Nautiyal_at_2015_Filmfare_Awards.jpg/220px-Jubin_Nautiyal_at_2015_Filmfare_Awards.jpg"
+},
+{
+    id:"s15",
+    title:"Kinna Sona",
+    artistId:"a5",
+    artistName:"Jubin Nautiyal",
+    albumId:"a5",
+    albumName:"Jubin Nautiyal Hits",
+    duration:"4:00",
+    image:"https://upload.wikimedia.org/wikipedia/commons/thumb/1/1e/Jubin_Nautiyal_at_2015_Filmfare_Awards.jpg/220px-Jubin_Nautiyal_at_2015_Filmfare_Awards.jpg"
+},
+{
+    id:"s16",
+    title:"Tujhe Kitna Chahne Lage",
+    artistId:"a5",
+    artistName:"Jubin Nautiyal",
+    albumId:"a5",
+    albumName:"Jubin Nautiyal Hits",
+    duration:"4:45",
+    image:"https://upload.wikimedia.org/wikipedia/commons/thumb/1/1e/Jubin_Nautiyal_at_2015_Filmfare_Awards.jpg/220px-Jubin_Nautiyal_at_2015_Filmfare_Awards.jpg"
+},
+{
+    id:"s17",
+    title:"Dilbar",
+    artistId:"a6",
+    artistName:"Neha Kakkar",
+    albumId:"a6",   
+    albumName:"Neha Kakkar Hits",
+    duration:"3:50",
+    image:"https://upload.wikimedia.org/wikipedia/commons/thumb/1/1e/Neha_Kakkar_at_2015_Filmfare_Awards.jpg/220px-Neha_Kakkar_at_2015_Filmfare_Awards.jpg"
+},
+{
+    id:"s18",
+    title:"Aankh Marey",
+    artistId:"a6",
+    artistName:"Neha Kakkar",
+    albumId:"a6",   
+    albumName:"Neha Kakkar Hits",
+    duration:"3:30",
+    image:"https://upload.wikimedia.org/wikipedia/commons/thumb/1/1e/Neha_Kakkar_at_2015_Filmfare_Awards.jpg/220px-Neha_Kakkar_at_2015_Filmfare_Awards.jpg"
+},
+{
+    id:"s19",
+    title:"O Saki Saki",
+    artistId:"a6",
+    artistName:"Neha Kakkar",
+    albumId:"a6",   
+    albumName:"Neha Kakkar Hits",
+    duration:"4:00",
+    image:"https://upload.wikimedia.org/wikipedia/commons/thumb/1/1e/Neha_Kakkar_at_2015_Filmfare_Awards.jpg/220px-Neha_Kakkar_at_2015_Filmfare_Awards.jpg"
+
+},
+{
+    id:"s20",
+    title:"pagaal",
+    artistId:"a7",
+    artistName:"Badshah",
+    albumId:"a7",   
+    albumName:"Badshah Hits",
+    duration:"3:40",
+    image:"https://upload.wikimedia.org/wikipedia/commons/thumb/1/1e/Badshah_at_2015_Filmfare_Awards.jpg/220px-Badshah_at_2015_Filmfare_Awards.jpg"
+},
+{
+    id:"s21",
+    title:"DJ Waley Babu",
+    artistId:"a7",
+    artistName:"Badshah",
+    albumId:"a7",   
+    albumName:"Badshah Hits",
+    duration:"3:20",
+    image:"https://upload.wikimedia.org/wikipedia/commons/thumb/1/1e/Badshah_at_2015_Filmfare_Awards.jpg/220px-Badshah_at_2015_Filmfare_Awards.jpg"
+},
+{
+    id:"s22",
+    title:"Genda Phool",
+    artistId:"a7",
+    artistName:"Badshah",
+    albumId:"a7",   
+    albumName:"Badshah Hits",
+    duration:"4:10",
+    image:"https://upload.wikimedia.org/wikipedia/commons/thumb/1/1e/Badshah_at_2015_Filmfare_Awards.jpg/220px-Badshah_at_2015_Filmfare_Awards.jpg"
+},
+
+]
+export const PLAYLISTS = [
+  {
+    id: "p1",
+    name: "Bollywood Hits",
+    description: "Best of Bollywood all time",
+    image: "",
+    songs: ["s1", "s2", "s3", "s4", "s5"],
+  },
+  {
+    id: "p2",
+    name: "Romantic Vibes",
+    description: "Perfect songs for a romantic evening",
+    image: "",
+    songs: ["s1", "s3", "s8", "s11", "s14", "s16"],
+  },
+  {
+    id: "p3",
+    name: "AR Rahman Classics",
+    description: "Timeless hits by the Mozart of Madras",
+    image: "",
+    songs: ["s5", "s6", "s7"],
+  },
+  {
+    id: "p4",
+    name: "Party Songs",
+    description: "Get the party started",
+    image: "",
+    songs: ["s17", "s18", "s19", "s20", "s21", "s22"],
+  },
+  {
+    id: "p5",
+    name: "Shreya Specials",
+    description: "Melodious hits by Shreya Ghoshal",
+    image: "",
+    songs: ["s8", "s9", "s10"],
+  },
+  {
+    id: "p6",
+    name: "90s Nostalgia",
+    description: "Throwback to the golden era",
+    image: "",
+    songs: ["s11", "s12", "s13", "s6", "s7"],
+  },
+  {
+    id: "p7",
+    name: "Morning Motivation",
+    description: "Start your day right",
+    image: "",
+    songs: ["s5", "s14", "s15", "s1", "s20"],
+  },
+  {
+    id: "p8",
+    name: "Arijit Singh Best",
+    description: "Top tracks by Arijit Singh",
+    image: "",
+    songs: ["s1", "s2", "s3", "s4"],
+  },
+];
+
+export const getSongById = (id) => {
+    return SONGS.find((song)=> song.id === id);
+};
+
+export const getArtistById = (id) => {
+  return ARTISTS.find((artist) => artist.id === id);
+};
+
+export const getSongsByArtist = (artistId) => {
+  return SONGS.filter((song) => song.artistId === artistId);
+};
+
+export const getSongsByAlbum = (albumId) => {
+  return SONGS.filter((song) => song.albumId   === albumId);
+};
